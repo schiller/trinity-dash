d36415704c2cc0dfaae598bacb3e2f7d5821df0f
schiller <schillerbr@gmail.com>
2017-06-25 01:26:44 +0100

    added csv locally

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ server = flask.Flask('app')
 
 app = dash.Dash('app', server=server)
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/'
-    'cb5392c35661370d95f300086accea51/raw/'
-    '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
-    'indicators.csv')
+# df = pd.read_csv(
+#     'https://gist.githubusercontent.com/chriddyp/'
+#     'cb5392c35661370d95f300086accea51/raw/'
+#     '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
+#     'indicators.csv')
+
+df = pd.read_csv('indicators.csv')
 
 available_indicators = df['Indicator Name'].unique()
 
